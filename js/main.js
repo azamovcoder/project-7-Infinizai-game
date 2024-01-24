46bf8bb1b06b71965aa1d64d446ecaaa93a23f07
azamovcoder <azamovcoder@gmail.com>
2024-01-24 08:04:56 +0300

    not all

---
+++ js/main.js
@@ -0,0 +1,24 @@
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+		navbar.classList.remove("navbar-shrink");
+	}
+}
+
+let modeBtn = document.getElementById("mode-btn");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "light") {
+    this.firstElementChild.src = "images/dark.svg";
+  } else {
+    this.firstElementChild.src = "images/light.svg";
+  }
+  document.body.classList.toggle("light");
+});
